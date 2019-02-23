43545147d776ce108dfdad5fad6c8ea1eed52d53
ikukadia <kukadia2@illinois.edu>
2019-02-23 04:11:25 -0600

    added minor name to tuple

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-
-def get_sites():
+def get_minor_sites():
     page = requests.get('http://catalog.illinois.edu/undergraduate/minors/')
     soup = BeautifulSoup(page.content, 'html.parser')
     sites = []
@@ -11,13 +10,14 @@ def get_sites():
     for rows in table:
         items = rows.findAll("li")
         for item in items:
-            sites.append(base_url + item.a.get("href"))
+            tup = ()
+            tup = tup + (item.find('a').text,)
+            tup = tup + (base_url + item.a.get("href"),)
+            sites.append(tup)
     return sites
-
 
 def is_course_row(css_class):
     return css_class == "even" or css_class == "odd"
-
 
 def getCredit(url):
     page = requests.get(url).text
@@ -25,9 +25,9 @@ def getCredit(url):
     search_result = soup.find("div", class_="searchresult").h2.getText()
     return search_result[search_result.find("credit: ") + len("credit: ")]
 
-def get_course_requirements(url):
+def get_course_requirements(site):
     courses = []
-    page = requests.get(url).text
+    page = requests.get(site[1]).text
     soup = BeautifulSoup(page, 'html.parser')
     tables = soup.findAll("table", class_="sc_courselist")
     for table in tables:
@@ -37,27 +37,21 @@ def get_course_requirements(url):
                 cols = row.findAll("td", class_="codecol")
                 base_url = "http://catalog.illinois.edu"
                 for i, col in enumerate(cols):
-                    req = req + (col.getText().replace("\xa0", ""),)
+                    req = req + (site[0],)
+                    req = req + (col.getText().replace("\xa0", " "),)
                     url = base_url + col.a.get("href")
                     req = req + (getCredit(url),)
                 cols = row.findAll("td", class_=None)
                 for i, col in enumerate(cols):
                     req = req + (col.getText(),)
-                    
-                if len(req) == 3:
+                if len(req) == 4:
                     courses.append(req)
     return courses
 
-
-    
-
 list = []
-
-sites = get_sites()
-for site in sites:
+sites = get_minor_sites()
+for i, site in enumerate(sites):
     req = get_course_requirements(site)
     if req is not None:
         list.append(req)
-        
-   
 print(list)
