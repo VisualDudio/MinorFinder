eab8bd899ba4ebb597dd56d05a8526e36f27a5f8
ikukadia <kukadia2@illinois.edu>
2019-02-23 03:13:30 -0600

    gets url for credit hrs

--- main.py
+++ main.py
@@ -16,30 +16,28 @@ def get_sites():
 def is_course_row(css_class):
     return css_class == "even" or css_class == "odd"
 
-
 def get_course_requirements(url):
     courses = []
 
     page = requests.get(url).text
     soup = BeautifulSoup(page, 'html.parser')
     
-
     tables = soup.findAll("table", class_="sc_courselist")
     for table in tables:
         rows = table.findAll("tr", class_=is_course_row)
         for row in rows:
             req = ()
             cols = row.findAll("td", class_="codecol")
+            base_url = "http://catalog.illinois.edu"
             for i, col in enumerate(cols):
                 req = req + (col.getText().replace("\xa0", ""),)
+                url = base_url + col.a.get("href")
+                req = req + getCredit(url)
             cols = row.findAll("td", class_=None)
             for i, col in enumerate(cols):
                 req = req + (col.getText(),)
             if len(req) == 2:
-                courses.append(req)
-    print(courses)
-   
-
+                courses.append(req)   
 
 url = "http://catalog.illinois.edu/undergraduate/las/academic-units/math/mathematics-minor/"
 get_course_requirements(url)
