317d268c6ce06a8c138df4542b889362daa193ca
ikukadia <kukadia2@illinois.edu>
2019-02-23 01:27:42 -0600

    parsed minor sites from catalog

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
-def is_course_row(css_class):
-    return css_class == "even" or css_class == "odd"
-
-page = requests.get("http://catalog.illinois.edu/undergraduate/engineer/departments/comp-sci/#text")
-soup = BeautifulSoup(page.content, 'html.parser')
-
-tables = soup.findAll("table", class_="sc_courselist")
-
-for table in tables:
-    rows = table.findAll("tr", class_=is_course_row)
-    print(rows)
-    break
+def get_sites():
+    page = requests.get('http://catalog.illinois.edu/undergraduate/minors/')
+    soup = BeautifulSoup(page.content, 'html.parser')
+    sites = []
+    base_url = "http://catalog.illinois.edu"
+    table = soup.findAll("div", {"class": "tab_content"})
+    for rows in table:
+        items = rows.findAll("li")
+        for item in items:
+            sites.append(base_url + item.a.get("href"))
+    return sites
