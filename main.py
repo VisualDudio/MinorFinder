4315a6cad03e38dd4e98771910e34aa57398d4d3
Aishik Ghosh <aishikg2@illinois.edu>
2019-02-23 03:03:14 -0600

    Gets course tuples

--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 import requests
 from bs4 import BeautifulSoup
 
+
 def is_course_row(css_class):
     return css_class == "even" or css_class == "odd"
 
-page = requests.get("http://catalog.illinois.edu/undergraduate/engineer/departments/comp-sci/#text")
-soup = BeautifulSoup(page.content, 'html.parser')
 
-tables = soup.findAll("table", class_="sc_courselist")
+def get_course_requirements(url):
+    courses = []
 
-for table in tables:
-    rows = table.findAll("tr", class_=is_course_row)
-    print(rows)
-    break
+    page = requests.get(url).text
+    soup = BeautifulSoup(page, 'html.parser')
+    
+
+    tables = soup.findAll("table", class_="sc_courselist")
+    for table in tables:
+        rows = table.findAll("tr", class_=is_course_row)
+        for row in rows:
+            req = ()
+            cols = row.findAll("td", class_="codecol")
+            for i, col in enumerate(cols):
+                req = req + (col.getText().replace("\xa0", ""),)
+            cols = row.findAll("td", class_=None)
+            for i, col in enumerate(cols):
+                req = req + (col.getText(),)
+            if len(req) == 2:
+                courses.append(req)
+    print(courses)
+   
+
+
+url = "http://catalog.illinois.edu/undergraduate/las/academic-units/math/mathematics-minor/"
+get_course_requirements(url)
